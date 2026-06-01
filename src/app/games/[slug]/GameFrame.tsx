ad028602921081cef02cc10a8832e3b1f1162441
Valzz <jkt48connect@gmail.com>
2026-06-01 16:08:18 +0700

    Update GameFrame.tsx

--- src/app/games/[slug]/GameFrame.tsx
+++ src/app/games/[slug]/GameFrame.tsx
@@ -29,7 +29,7 @@ export default function GameFrame({ src, title, showMusicToggle }: GameFrameProp
   };
 
   return (
-    <div style={{ position: "relative", width: "100%", height: "100vh", overflow: "hidden" }}>
+    <div style={{ position: "relative", width: "100%", height: "100%" }}>
       {showMusicToggle && (
         <button
           onClick={toggleMusic}
